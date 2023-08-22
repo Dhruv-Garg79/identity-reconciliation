830b6674e21b5f7a8abc684a2e5e873e0dc5378c
Dhruv Garg <garg.dh123@gmail.com>
2023-08-22 23:57:47 +0530

    fix: port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import Logger from './utils/logger';
 
 const logger = new Logger('server');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
 	logger.info(`Listening to port ${port}`);
